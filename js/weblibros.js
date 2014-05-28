1189e2a7a75aa1417886490d9d79cfb5af556e44
LouK <8thelouk8@gmail.com>
2014-05-28 14:05:37 +0200

    Updated weblibros.js

--- js/weblibros.js
+++ js/weblibros.js
@@ -1,7 +1,24 @@
-$(".libro-img").mouseover(function() {
-  id = $(this).attr('id');
-  $( ".libro-head" ).css("visibility", "visible");
+var clicked = false;
+$(".libro-box").click(function() {
+    if (!clicked) {
+        id = $(this).attr('id');
+        $( ".libro-box" ).filter("#"+id).finish();
+        $( ".libro-box" ).filter("#"+id).animate({
+            width: $( ".libro-box" ).filter("#"+id).width()+300
+        }, 200, function() {
+            $( ".libro-head" ).filter("#"+id).fadeIn( "slow" , 0, function() {});
+        }); 
+        clicked = true
+    }
 }).mouseleave(function() {
-  id = $(this).attr('id');
-  $( ".libro-head" ).css("visibility", "hidden");
+    if (clicked) {
+        id = $(this).attr('id');
+        $( ".libro-box" ).filter("#"+id).finish();
+        $( ".libro-head" ).filter("#"+id).css("display", "none");
+        $( ".libro-box" ).filter("#"+id).animate({
+            width: $( ".libro-box" ).filter("#"+id).width()-300
+        }, 200, function() {
+        }); 
+        clicked = false
+    }
 });
