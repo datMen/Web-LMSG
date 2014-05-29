7d7579868f718820b4363b4baa651c762525a30e
TheLouK <8thelouk8@gmail.com>
2014-05-29 14:24:43 +0100

    Better way to open .box-head checking "display" css property

--- js/weblibros.js
+++ js/weblibros.js
@@ -1,23 +1,18 @@
-var clicked = false;
 $(".libro-box").click(function() {
     id = $(this).attr('id');
-    if (!clicked) {
+    if ($( ".libro-head" ).filter("#"+id).css("display") == "none") {
         $( ".libro-box" ).filter("#"+id).finish();
         $( ".libro-box" ).filter("#"+id).animate({
             width: $( ".libro-head" ).filter("#"+id).width()+($( ".libro-img" ).filter("#"+id).width()*2)
         }, 200, function() {
             $( ".libro-head" ).filter("#"+id).fadeIn( "slow" , 0, function() {});
         });
-        clicked = true;
     }
-}).mouseleave(function() {
-    id = $(this).attr('id');
-    if (clicked) {
+    else if ($( ".libro-head" ).filter("#"+id).css("display") == "block") {
         $( ".libro-box" ).filter("#"+id).finish();
         $( ".libro-head" ).filter("#"+id).css("display", "none");
         $( ".libro-box" ).filter("#"+id).animate({
             width:  $( ".libro-img" ).filter("#"+id).width()
         }, 200);
-        clicked = false;
     }
 });
